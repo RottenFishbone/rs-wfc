7705547d4a17b55f4dbcc3eb9b0a75a2c8931898
Jayden Dumouchel <jdumouch@ualberta.ca>
2022-11-25 19:02:58 -0700

    Restructured kernels to be less monolithic Implemented several steps to parallel WFC

--- resources/kernel.cu
+++ resources/kernel.cu
@@ -1,154 +1,175 @@
 #include <stdlib.h>
-#include <cooperative_groups.h>
+#include <stdint.h>
 
-// `flags` is a group of bitfields for each cell in the map
-// bit 0    -- Cell is collapsed
-extern __shared__ int flags[];
-
-extern "C" typedef enum {
+#define NUM_DIRS 4
+typedef enum {
     UP, DOWN, LEFT, RIGHT
 } Direction;
 
-// Up down left right
 
-extern "C" __device__ int findConstraints(int *domain, int *constraints, 
-        int numConstraints, int relativeDir) {
+/**
+    @brief Calculate imposed constraints by a cell's current domain
+
+    Determines the imposed constraints in a specified direction
+    from a specified cell. Uses each possible value in `cell` to union
+    the rules together from `constraints` in the direction `dir`.
+
+    The output should be intersected by the cell in direction `dir` to
+    constrain its domain to be arc consistent.
     
-    int allowedDomain = 0;
-    int tmpDomain = *domain;
-    int i=0;
-    // TODO potentially optimize
-    while (tmpDomain) {
-        // If the currently viewed bit is set then tile 'i' is in the domain
-        if (tmpDomain & 1){
-            // We need to union the allowed values based on this fact
-            allowedDomain |= constraints[i*4 + relativeDir];
-        }
-        // Advance the cursor in the bitfield
-        tmpDomain>>=1;
-        ++i;
-    }
-    return allowedDomain;
-}
-
-extern "C" __device__ void propagate(int *buffer0, int *buffer1, int *constraints, 
-    int numConstraints, int width, int height, int x, int y, int *activeBuffer) {
-    __shared__ int propagating;
-
+    @param cell A pointer to the imposing cell's domain
+    @param constraints A pointer to the array of constraints
+    @param dir The direction the cell is imposing constraints
+    @return Complete set of allowed values for domain in direction `dir`
+ */
+extern "C" 
+__device__ 
+inline
+int findConstraints(int32_t *cell, int32_t *constraints, Direction dir) {
+    if (!(*cell)) { return; }
+    int validDomain, offset, i;
+    
+    // `i` is the amount to shift `*cell` to have bit0 be a set bit
+    // By extension, `i` also represents the tile id we are concerned with
+    // Note: __ffs is used to provide hardware level integer intrinsics
+    // and brings the count from worst case 30 wasted ops to 0 wasted ops
+    i = __ffs(*cell)-1;
+    // `validDomain` is the domain the cell in direction `dir` is allowed to be
+    validDomain = 0;
     do {
-        propagating = 0;
-        __syncthreads();
 
-        // Swap buffers if needed
-        if (!((*activeBuffer)%2)){
-            int *tmp = buffer0;
-            buffer0 = buffer1;
-            buffer1 = tmp;
-        }
+        // Union the possible domains of each tile to determine the possible
+        // domain of the cell in direction `dir`
+        validDomain |= constraints[i*NUM_DIRS + dir];
         
-        int *domain = &buffer0[x+y*width];
-        int origDomain = *domain;
-        // Intersect the restricted domain of each neighbour
-        if (x > 0) {
-            *domain &= findConstraints(
-                    &buffer0[x-1 + y*width],
-                    constraints,
-                    numConstraints,
-                    RIGHT);   // Neighbour is left, relativeDir is right (3)
-        }
-        if (x < width-1){
-            *domain &= findConstraints(
-                    &buffer0[x+1 + y*width],
-                    constraints,
-                    numConstraints,
-                    LEFT);
-        }
-        if (y > 0) {
-            *domain &= findConstraints(
-                    &buffer0[x + (y-1)*width],
-                    constraints,
-                    numConstraints,
-                    DOWN);
-        }
-        if (y < height-1) {
-            *domain &= findConstraints(
-                    &buffer0[x + (y+1)*width],
-                    constraints,
-                    numConstraints,
-                    UP);
-        }
-
-        if (origDomain != *domain) { propagating = 1; }
+        // Find the distance to the next set bit
+        offset = __ffs((*cell) >> i+1);
+        i += offset;
         
-        __syncthreads();
-        if (x == 0 && y == 0){
-            ++(*activeBuffer);
-        }
-        __syncthreads();
+        // Repeat if there is a next set bit
+    } while (offset != 0);
 
-        // Propagate again until none are queued
-    } while (propagating);     
+    return validDomain;
 }
 
+extern "C" 
+__global__ 
+void iterate_ac(int32_t *domains, int32_t *constraints,
+        int32_t width, int32_t height, int32_t *changesOccured){
 
-
-extern "C" __global__ void collapse(int *buffer0, int *buffer1, int *constraints, 
-        int numConstraints, int width, int height, int *resultBuffer){
-    
-    // blockCollapsed is a flag to denote every thread is assigned to a collapsed cell (end cond.)
-    __shared__ int blockCollapsed;
-    // TODO enable use of blocks, this will require block level synchronization
-    int x = (threadIdx.x + blockDim.x * blockIdx.x) % width; 
-    int y = (threadIdx.x + blockDim.x * blockIdx.x) / width; 
-    if (x >= width || y >= height) { return; } // Ignore out of bounds threads
-    
-    // Init values before starting
-    blockCollapsed = 0; // Each thread inits this but that doesn't matter due to sync
-    flags[x+y*width] &= ~1;
+    __shared__ int changed;
+    changed = 0;
     __syncthreads();
 
+    int32_t x = (threadIdx.x + blockDim.x * blockIdx.x) % width; 
+    int32_t y = (threadIdx.x + blockDim.x * blockIdx.x) / width; 
+    if (x >= width || y >= height) { return; }
 
-    while (!blockCollapsed) {
-        // Call a propagation step, this is done until all values are stable
-        propagate(buffer0, buffer1, 
-                constraints, numConstraints, 
-                width, height, x, y, 
-                resultBuffer);
-        return; 
-        
-        // Perform a test that the block is collapsed
-        // TODO perform test as a parallel reduction
-        // NOTE: Does not work with blocks, as only thread level sync is performed
-        if (x == 0 && y == 0) {
-            blockCollapsed = 1;
-            for (int i = 0; i < width * height; ++i) {
-                // Skip cells with collapsed flag set
-                if (flags[i]&1) { continue; }
-                
-                // Clone the domain so we can bitshift it
-                int testVal = buffer0[i];
-                // If it is 0 we have issues (unsolvable)
-                // TODO error handling of invalid state, backtracking maybe
-                if (!testVal) { return; }
-
-                // Iterate over each bit by shifting right each iteration
-                while (testVal) {
-                    // Check if the 0th-bit and another are set
-                   if ((testVal & 1) && (testVal > 1)) {
-                       // Block is not collapsed in the event of multiple set bits
-                        blockCollapsed = 0;
-                        goto doneTest;
-                    }
-
-                    testVal>>=1;
-                }
-                flags[i] |= 1;
-            }  
-doneTest:
-        }
-        __syncthreads();
+    int32_t *domain = &domains[x+y*width];
+    int32_t origDomain = *domain;
+    // Intersect the restricted domain of each neighbour
+    if (x > 0) {
+        *domain &= findConstraints(
+                &domains[x-1 + y*width],
+                constraints,
+                RIGHT);   // Neighbour is left, relativeDir is right (3)
     }
-    
+    if (x < width-1){
+        *domain &= findConstraints(
+                &domains[x+1 + y*width],
+                constraints,
+                LEFT);
+    }
+    if (y > 0) {
+        *domain &= findConstraints(
+                &domains[x + (y-1)*width],
+                constraints,
+                DOWN);
+    }
+    if (y < height-1) {
+        *domain &= findConstraints(
+                &domains[x + (y+1)*width],
+                constraints,
+                UP);
+    }
+
+    // Set changesOccured return flag to 1 if the domain was altered
+    if (origDomain != *domain) { changed = 1; }
+
+    // Minimize memory access to global memory by only having 1 thread per
+    // block attempt to set it
+    __syncthreads();
+    if (threadIdx.x + threadIdx.y == 0 && changed) {
+        *changesOccured = 1;
+    }
+}
+
+/**
+  @brief Construct a vector of element id's where `counts[id] == searchVal`
+
+  @param counts The array of values to search
+  @param searchVal The value to search for
+  @param results The buffer to store the found ids
+  @param counter The length of the vector
+*/
+extern "C"
+__global__
+void collect_ids(uint32_t *counts, uint32_t searchVal, uint32_t length, 
+        uint32_t *results, uint32_t *resCount){
+
+    uint32_t i = threadIdx.x + blockDim.x * blockIdx.x;
+    if (i >= length || counts[i] != searchVal) { return; }
+    results[atomicAdd(resCount,1)] = i;
 }
 
 
+/**
+  @brief Counts the length of each domain and outputs the result into a new buffer
+
+  @param domains The array containing bitfields representing domain values
+  @param results The output buffer to store the # of values in corres. domain
+  @param length The length of both the domains and the buffer.
+*/
+extern "C"
+__global__
+void count_domains(int32_t *domains, uint32_t *results, uint32_t length){
+    uint32_t i = threadIdx.x + blockDim.x * blockIdx.x;
+    if (i >= length) { return; } // Prevent OOB access
+    results[i] = __popc(domains[i]); // __popc provides hardware level bit counting
+}
+
+extern "C"
+__global__
+void bit_count_min(uint32_t *counts, uint32_t *results, uint32_t length, uint32_t *result){
+    extern __shared__ uint32_t s_results[];
+
+    uint32_t i = threadIdx.x;
+    uint32_t minVal = counts[i + blockIdx.x*blockDim.x];
+    
+    if (i + blockDim.x < length) {
+        minVal = min(counts[i+blockDim.x], minVal);
+    }
+
+    s_results[i] = minVal;
+    __syncthreads();
+
+    // Continually cut the stride in half, only allowing threads within
+    // the stride to continue reduction. Eventually the block converges
+    // to a single thread.
+    for (uint32_t stride=blockDim.x/2; stride > 0; stride /= 2) {
+        if (i < stride) {
+            minVal = min(s_results[i + stride], minVal);
+            s_results[i] = minVal;
+        }
+        __syncthreads();
+    }
+
+    // Write the result of the block into the global result
+    // Each block writes into its corresponding index
+    if (i == 0) {
+        results[blockIdx.x] = minVal;
+        // Also emit a single value, this is only useful on the last 
+        // kernel launch
+        *result = minVal;
+    }
+}
--- src/ac3cuda.rs
+++ src/ac3cuda.rs
@@ -3,13 +3,13 @@
 
 use std::{ffi::CString, fmt::Display};
 
-use rustacuda::prelude::*;
+use rustacuda::{prelude::*, error::CudaError};
 use rustacuda::memory::DeviceBox;
+use rustacuda_core::DevicePointer;
 use std::collections::HashSet;
 
 use crate::datatype::{Vec2, Map, Tilemap};
 pub fn collapse_from_sample(sample: &Map<i32>, output_size: Vec2) -> Tilemap {
-    println!("{}", Constraints::from(sample));
     CudaWavemap::collapse_from_sample(sample, output_size)
 }
 
@@ -60,6 +60,8 @@ impl From<&Tilemap> for Constraints {
 struct CudaWavemap(Map<i32>);
 impl CudaWavemap {
     fn collapse_from_sample(sample: &Map<i32>, output_size: Vec2) -> Tilemap {
+        let block_size = 512;
+
         rustacuda::init(CudaFlags::empty()).unwrap();
         let device = Device::get_device(0).unwrap();
         let _context = Context::create_and_push(ContextFlags::MAP_HOST, device).unwrap();
@@ -71,18 +73,19 @@ impl CudaWavemap {
 
         let stream = Stream::new(StreamFlags::NON_BLOCKING, None).unwrap();
         
-        let mut output_map: Map<i32> = Map::new(output_size, Some(7));
-        output_map.data[0] = 1<<2;
-        output_map.data[2] = 1<<0;
-        output_map.data[(output_size.x*2) as usize] = 1<<0;
-        let mut buffer1_dev = unsafe {
+        let mut output_map: Map<u32> = Map::new(output_size, Some(999));
+        
+        //output_map.data[0] = 1<<2;
+        //output_map.data[1] = 1<<2;
+        //output_map.data[2] = 1<<0;
+        //output_map.data[(output_size.x*2) as usize] = 1<<0;
+        //output_map.data[(output_size.x*output_size.y) as usize - 1] = 1<<1;
+        //output_map.data[((output_size.x)*(output_size.y-1)) as usize - 2] = 1<<0;
+        
+        let mut buffer_dev = unsafe {
             DeviceBuffer::uninitialized(output_map.data.len()).unwrap()
         };
-        let mut buffer2_dev = unsafe {
-            DeviceBuffer::uninitialized(output_map.data.len()).unwrap()
-        };
-        buffer1_dev.copy_from(&output_map.data[..]).unwrap();
-        buffer2_dev.copy_from(&output_map.data[..]).unwrap();
+        buffer_dev.copy_from(&output_map.data[..]).unwrap();
         
         let constraints = Constraints::from(sample);
         let constraints: Vec<i32> = constraints.0.into_iter().flatten().collect();
@@ -90,34 +93,120 @@ impl CudaWavemap {
             DeviceBuffer::uninitialized(constraints.len()).unwrap()
         };
         constraints_dev.copy_from(&constraints[..]).unwrap();
-        let mut res_buffer_dev = DeviceBox::new(&0_u32).unwrap();
-
-        unsafe {
-            let dims = (output_size.x as u32, output_size.y as u32);
-            launch!(module.collapse<<<10, 512, (dims.0*dims.1*4), stream>>>(
-                buffer1_dev.as_device_ptr(),
-                buffer2_dev.as_device_ptr(),
-                constraints_dev.as_device_ptr(),
-                constraints.len()/4,
-                output_map.size.x,
-                output_map.size.y,
-                res_buffer_dev.as_device_ptr()
-            )).unwrap();
-        }
-
-        stream.synchronize().unwrap();
-
-        let mut res_in_buffer_host = 0;
-        res_buffer_dev.copy_to(&mut res_in_buffer_host).unwrap();
-        if res_in_buffer_host == 0 {
-            buffer1_dev.copy_to(&mut output_map.data).unwrap();
-        } else {
-            buffer2_dev.copy_to(&mut output_map.data).unwrap();
-        }
-        println!("{}", output_map);
-        println!("{:?}", res_in_buffer_host);
         
+        let mut buffer0_dev = unsafe {
+            DeviceBuffer::uninitialized(output_map.data.len()).unwrap()
+        };
+        buffer0_dev.copy_from(&output_map.data[..]).unwrap();
+        
+        let mut buffer1_dev = unsafe {
+            DeviceBuffer::uninitialized(output_map.data.len()).unwrap()
+        };
+        buffer1_dev.copy_from(&output_map.data[..]).unwrap();
+        
+
+        let mut changes_occured_dev = DeviceBox::new(&0_i32).unwrap();
+        
+        let dims = (output_size.x as u32, output_size.y as u32);
+        let size = dims.0 * dims.1;
+        let blocks_needed = (size+block_size-1)/block_size;
+
+        let mut changes_occured: i32 = 1;
+        let mut call_count: u32 = 0;
+        let mut result = 0;
+        while changes_occured == 1 {
+            // Default device state to 'no changes'
+            changes_occured_dev.copy_from(&0_i32).unwrap();
+            // Run a single iteration of parallel AC3
+            unsafe { 
+                launch!(
+                    module.iterate_ac<<<blocks_needed, block_size, 0, stream>>>(
+                        buffer_dev.as_device_ptr(),
+                        constraints_dev.as_device_ptr(),
+                        output_map.size.x, output_map.size.y,
+                        changes_occured_dev.as_device_ptr()
+                    )
+                ).unwrap();
+            }
+            stream.synchronize().unwrap(); // Sync grid
+
+            // Transfer shared memory to host to test if changes occured
+            changes_occured_dev.copy_to(&mut changes_occured).unwrap();
+            call_count+=1;   
+            unsafe {
+                result = CudaWavemap::parallel_min_domain(
+                    size, &module, &stream, 
+                    buffer_dev.as_device_ptr(),
+                    buffer0_dev.as_device_ptr(),
+                    buffer1_dev.as_device_ptr()).unwrap();
+            }
+        }
+
+
+        buffer1_dev.copy_to(&mut output_map.data).unwrap();
+        //println!("Domain sizes: {}", output_map);
+        buffer_dev.copy_to(&mut output_map.data).unwrap();
+        //println!("State (bitfields): {}", output_map);
+        println!("blocks: {}x{} threads.\nTotal threads: {}", 
+            blocks_needed, block_size, block_size*blocks_needed);
+        println!("Called: {} times", call_count);
+
+        println!("Smallest domain: {}", result);
         Tilemap::new(output_size, None)
+    }
+
+    //TODO select random cell using collect_ids kernel in unified memory
+
+    /**
+     * Performs parallel reduction in CUDA kernels to find the minimum and maximum
+     * domain size at the same time.
+     *
+     * `count_buffer*` should be the same size as the `domain_buffer`.
+     * One or both `count_buffer*`s will be modified.
+     */
+    unsafe fn parallel_min_domain(size: u32,
+        module: &Module, stream: &Stream,
+        domain_buffer: DevicePointer<u32>,
+        count_buffer0: DevicePointer<u32>,
+        count_buffer1: DevicePointer<u32>) 
+        -> Result<u32, CudaError> {
+        
+        let block_size = 512;
+        let mut blocks_count = (size+block_size-1)/block_size;
+
+        let mut final_val_dev = DeviceBox::new(&u32::MAX)?;
+
+        launch!(module.count_domains<<<blocks_count, block_size, 0, stream>>>
+            (domain_buffer,count_buffer0,size))?;
+        stream.synchronize()?;
+
+        launch!(module.bit_count_min<<<blocks_count, block_size, 4*block_size, stream>>>
+            (count_buffer0, count_buffer1, size, 
+             final_val_dev.as_device_ptr()))?;
+        stream.synchronize()?;
+        
+        // When true, denotes that buffer1 contains the data values
+        let mut swap_buffers = true;
+        
+        // Continue reducing the values in place until reduction is performed within
+        // a single block.
+        while blocks_count > 1 {
+            let prev_count = blocks_count;
+            blocks_count += block_size-1;
+            blocks_count /= block_size;
+
+            let buf0 = if swap_buffers { count_buffer1 } else { count_buffer0 };
+            let buf1 = if swap_buffers { count_buffer0 } else { count_buffer1 };
+            launch!(module.bit_count_min<<<blocks_count, block_size, 4*block_size, stream>>>
+                (buf0, buf1, prev_count, final_val_dev.as_device_ptr()))?;
+            stream.synchronize()?;
+            swap_buffers = !swap_buffers;
+        }
+        
+        let mut final_result = u32::MAX;
+        final_val_dev.copy_to(&mut final_result)?;
+
+        Ok(final_result)
     }
 }
 
