dd6e23d30cd8920e50e0f546b72ffe401fe73ba2
Jayden Dumouchel <jdumouch@ualberta.ca>
2022-11-29 14:14:25 -0700

    CUDA kernels build to require compute 6.0

--- build.rs
+++ build.rs
@@ -3,7 +3,8 @@ use std::process::Command;
 fn main() {
     println!("cargo:rerun-if-changed=resources/kernel.cu");
     match Command::new("nvcc")
-        .args(&["-ptx", "resources/kernel.cu", "-o", "resources/kernel.ptx"])
+        .args(&["-ptx", "resources/kernel.cu", "-o", "resources/kernel.ptx",
+                "-arch=sm_60"])
         .status() {
             Ok(msg) => {
                 if !msg.success() {
